75648b62639390515d30c5093b24d5791c9ff0ad
daniel.toth@everit.biz <daniel.toth@everit.biz>
2020-01-20 22:57:27 +0100

    Update About scene

--- app/src/main/java/com/watabou/pixeldungeon/scenes/AboutScene.java
+++ app/src/main/java/com/watabou/pixeldungeon/scenes/AboutScene.java
@@ -36,17 +36,24 @@ import android.net.Uri;
 public class AboutScene extends PixelScene {
 
   private static final String TXT =
-      "Code & graphics: Watabou\n" +
-          "Music: Cube_Code\n\n" +
-          "This game is inspired by Brian Walker's Brogue. " +
-          "Try it on Windows, Mac OS or Linux - it's awesome! ;)\n\n" +
-          "Please visit official website for additional info:";
+                  "Code & graphics: Novalis Denahi\n" +
+                  "Original Code & graphics: Watabou\n" +
+                  "Music: Cube_Code\n\n" +
+                  "This game is inspired by Watabou's Pixel Dungeon. " +
+                  "Basically it's the same game with a few extra stuff. " +
+                  "I hope you will enjoy them.\n\n" +
+                  "Please consider to support the original game creator and check " +
+                          "out the awesome inspiring works of Watabou:";
 
-  private static final String LNK = "pixeldungeon.watabou.ru";
+  private static final String LINK_ITCH = "watabou.itch.io/";
+  private static final String LINK_PATREON = "patreon.com/watawatabou";
 
   @Override
   public void create() {
     super.create();
+
+    Image wata = Icons.WATA.get();
+    Image estera = Icons.PRIEST.get();
 
     BitmapTextMultiline text = PixelScene.createMultiline(TXT, 8);
     text.maxWidth = Math.min(Camera.main.width, 120);
@@ -54,32 +61,57 @@ public class AboutScene extends PixelScene {
     add(text);
 
     text.x = PixelScene.align((Camera.main.width - text.width()) / 2);
-    text.y = PixelScene.align((Camera.main.height - text.height()) / 2);
+    text.y = PixelScene.align((Camera.main.height - text.height()) / 2 - wata.height());
 
-    BitmapTextMultiline link = PixelScene.createMultiline(LNK, 8);
-    link.maxWidth = Math.min(Camera.main.width, 120);
-    link.measure();
-    link.hardlight(Window.TITLE_COLOR);
-    add(link);
+    BitmapTextMultiline linkITCH = PixelScene.createMultiline(LINK_ITCH, 8);
+    linkITCH.maxWidth = Math.min(Camera.main.width, 120);
+    linkITCH.measure();
+    linkITCH.hardlight(Window.TITLE_COLOR);
+    add(linkITCH);
 
-    link.x = text.x;
-    link.y = text.y + text.height();
+    linkITCH.x = text.x;
+    linkITCH.y = text.y + text.height();
 
-    TouchArea hotArea = new TouchArea(link) {
+    TouchArea hotAreaITCH = new TouchArea(linkITCH) {
       @Override
       protected void onClick(final Touch touch) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + LNK));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://"+LINK_ITCH));
         Game.instance.startActivity(intent);
       }
     };
-    add(hotArea);
+    add(hotAreaITCH);
 
-    Image wata = Icons.WATA.get();
+    BitmapTextMultiline linkPatreon = PixelScene.createMultiline(LINK_PATREON, 8);
+    linkPatreon.maxWidth = Math.min(Camera.main.width, 120);
+    linkPatreon.measure();
+    linkPatreon.hardlight(Window.TITLE_COLOR);
+    add(linkPatreon);
+
+    linkPatreon.x = text.x;
+    linkPatreon.y = linkITCH.y + linkITCH.height();
+
+    TouchArea hotAreaPatreon = new TouchArea(linkPatreon) {
+      @Override
+      protected void onClick(final Touch touch) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://"+LINK_PATREON));
+        Game.instance.startActivity(intent);
+      }
+    };
+    add(hotAreaPatreon);
+
+
     wata.x = PixelScene.align((Camera.main.width - wata.width) / 2);
     wata.y = text.y - wata.height - 8;
     add(wata);
 
     new Flare(7, 64).color(0x112233, true).show(wata, 0).angularSpeed = +20;
+
+    //TODO add medal with colors and stuff. or own icon
+    estera.x = PixelScene.align((Camera.main.width - estera.width) / 2);
+    estera.y = linkITCH.y + estera.height + 8;
+    add(estera);
+
+    new Flare(7, 64).color(0x666666, true).show(estera, 0).angularSpeed = +20;
 
     Archs archs = new Archs();
     archs.setSize(Camera.main.width, Camera.main.height);
